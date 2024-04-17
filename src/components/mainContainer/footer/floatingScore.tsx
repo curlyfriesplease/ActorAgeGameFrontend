04fd9374a58d31eafc6193a69025257b1769f7cb
curlyfriesplease <jmulhern85+github@gmail.com>
2024-04-17 18:22:58 +0100

    Floating score text color

--- src/components/mainContainer/footer/floatingScore.tsx
+++ src/components/mainContainer/footer/floatingScore.tsx
@@ -1,13 +1,36 @@
+import { clsx } from 'clsx';
+
 export const FloatingScore = ({ currentScore }: { currentScore: number }) => {
   const tailwindScoreColor = () => {
-    let textColor;
-    if (currentScore < 1) textColor = `text-red-50`;
-    else if (currentScore > 9) textColor = `text-red-950`;
-    else textColor = `text-red-${currentScore}00`;
-    console.log('textColor', textColor);
-    return textColor;
+    // Not the most elegant function, but sadly tailwind doesn't support a dynamically generated class name, it only accepts complete unbroken strings https://tailwindcss.com/docs/content-configuration#dynamic-class-names
+    switch (currentScore) {
+      case 0:
+        return 'text-red-50';
+      case 1:
+        return 'text-red-100';
+      case 2:
+        return 'text-red-200';
+      case 3:
+        return 'text-red-300';
+      case 4:
+        return 'text-red-400';
+      case 5:
+        return 'text-red-500';
+      case 6:
+        return 'text-red-600';
+      case 7:
+        return 'text-red-700';
+      case 8:
+        return 'text-red-800';
+      case 9:
+        return 'text-red-900';
+      default:
+        return 'text-red-950';
+    }
   };
 
+  const scoreColor = tailwindScoreColor();
+  console.log('scoreColor', scoreColor);
   return (
     <div
       id="floatingScore"
@@ -22,11 +45,11 @@ export const FloatingScore = ({ currentScore }: { currentScore: number }) => {
       font-jersey
       rounded-full
       backdrop-blur-xl
-      ${tailwindScoreColor()}
+      ${scoreColor}
       `}
     >
       <h4 className="text-2xl">SCORE</h4>
-      <h4 className="text-5xl">{currentScore}</h4>
+      <h4 className={clsx(tailwindScoreColor(), 'text-5xl')}>{currentScore}</h4>
     </div>
   );
 };
